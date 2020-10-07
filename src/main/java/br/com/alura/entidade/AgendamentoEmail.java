cdb0e052793486f64a13b66a0b26a9cf0609af0f
marcosdenisalves <marcos.denisjr@gmail.com>
2020-10-07 20:34:10 -0300

    Serializando classe de domínio

--- src/main/java/br/com/alura/entidade/AgendamentoEmail.java
+++ src/main/java/br/com/alura/entidade/AgendamentoEmail.java
@@ -1,5 +1,7 @@
 package br.com.alura.entidade;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,8 +10,9 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "agendamentoemail")
-public class AgendamentoEmail {
-
+public class AgendamentoEmail implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
