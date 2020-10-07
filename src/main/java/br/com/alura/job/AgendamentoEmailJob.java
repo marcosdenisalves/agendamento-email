d483d56326269944cbefe89cf3f8544068324748
marcosdenisalves <marcos.denisjr@gmail.com>
2020-10-07 20:35:21 -0300

    Configurando classe job para envio de e-mails para o repositório(fila)

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -2,9 +2,13 @@ package br.com.alura.job;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
@@ -15,12 +19,19 @@ public class AgendamentoEmailJob {
 	@Inject
 	private AgendamentoEmailServico servico;
 	
+	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+	private JMSContext context;
+	
+	@Resource(mappedName = "java:/jms/queue/EmailQueue")
+	private Queue queue;
+	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public void enviarEmail() {
 		List<AgendamentoEmail> listarPorNaoAgendado
 			= servico.listarPorNaoAgendado();
 		listarPorNaoAgendado.forEach(emailNaoAgendado -> {
-			servico.enviar(emailNaoAgendado);
+			context.createProducer().send(queue, emailNaoAgendado);
 			servico.alteraStatus(emailNaoAgendado);
 		});
 	}
