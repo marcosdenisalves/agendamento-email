9224197bcb9c52860efb32f2f726af8127dd2e08
marcosdenisalves <marcos.denisjr@gmail.com>
2020-10-06 21:52:33 -0300

    Acrescentando métodos para status de emails na camada de serviço

--- src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
+++ src/main/java/br/com/alura/servico/AgendamentoEmailServico.java
@@ -1,6 +1,7 @@
 package br.com.alura.servico;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -10,6 +11,8 @@ import br.com.alura.entidade.AgendamentoEmail;
 
 @Stateless
 public class AgendamentoEmailServico {
+	
+	private static final Logger LOGGER = Logger.getLogger(AgendamentoEmailServico.class.getName());
 	
 	@Inject
 	private AgendamentoEmailDAO dao;
@@ -21,5 +24,25 @@ public class AgendamentoEmailServico {
 	public void inserir(AgendamentoEmail agendamentoEmail) {
 		agendamentoEmail.setAgendado(false);
 		dao.inserir(agendamentoEmail);
+	}
+	
+	public List<AgendamentoEmail> listarPorNaoAgendado() {
+		return dao.listarPorNaoAgendado();
+	}
+	
+	public void alteraStatus(AgendamentoEmail agendamentoEmail) {
+		agendamentoEmail.setAgendado(true);
+		dao.alterarStatusAgendado(agendamentoEmail);
+	}
+	
+	public void enviar(AgendamentoEmail agendamentoEmail) {
+		try {
+			Thread.sleep(5000);
+			LOGGER.info("O e-mail do(a) usuário(a) " 
+				+ agendamentoEmail.getEmail() 
+					+ " foi enviado com sucesso!!!");
+		} catch (Exception e) {
+			LOGGER.warning(e.getMessage());
+		}
 	}
 }
